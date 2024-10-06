438a2003da512a1d2c6a78771cb0922ca7764d1d
mario <khantkokozaw382000@gmail.com>
2024-10-07 00:16:24 +0800

    next.js route test

---
+++ next-app/app/components/AddToCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <div>
+        <button className='btn btn-primary' onClick={()=> console.log("Heyyy")}>Click Me To Add</button>
+    </div>
+  )
+}
+
+export default AddToCard
---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+import style from './ProductCard.module.css'
+
+
+const ProductCard = () => {
+  return (
+    <div className= {style.card}>
+        <AddToCard/>
+    </div>
+  )
+}
+ 
+export default ProductCard
